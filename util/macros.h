c9a9ffb6803c16dbd0395867ee720ec63ede2839
Christopher Patton <chpatton013@gmail.com>
2017-02-24 22:40:04 -0800

    Util: simplify macro defn

--- util/macros.h
+++ util/macros.h
@@ -1,9 +1,9 @@
 #pragma once
 
+#include <boost/preprocessor.hpp>
+#include <glog/logging.h>
+
 #define CHECK_FAILED(expr, value)                                              \
   LOG(FATAL) << "Check failed: " expr ": " << value << ": "
 
-#define _DO_UNIQUE_VARIABLE_NAME_ELEVATE(x, y) x##y
-#define _DO_UNIQUE_VARIABLE_NAME(x, y) _DO_UNIQUE_VARIABLE_NAME_ELEVATE(x, y)
-#define UNIQUE_VARIABLE_NAME()                                                 \
-  _DO_UNIQUE_VARIABLE_NAME(__unique_variable_name_, __COUNTER__)
+#define UNIQUE_VARIABLE_NAME() BOOST_PP_CAT(_unique_variable_name_, __COUNTER__)
