901281b63dc82e08d4f09a786bb8d70d94163647
Christopher Patton <chpatton013@gmail.com>
2017-02-24 23:50:10 -0800

    Util: result support user errors

--- util/result.h
+++ util/result.h
@@ -2,6 +2,7 @@
 
 #include "trait/eq_ops.h"
 #include "trait/to_string.h"
+#include "util/enum_class.h"
 #include "util/expected.h"
 #include "util/macros.h"
 #include "util/static_string.h"
@@ -10,88 +11,86 @@
 
 namespace util {
 
-// TODO: Support user-supplied error codes.
-enum class ErrorCode {
-  Ok,
-  InvalidValue,
-  InvalidIndex,
-  AlreadyExists,
-  NotFound,
-  Unimplemented,
-  Unknown,
-};
-
-static constexpr StaticString ErrorCodeToName(const ErrorCode& code) noexcept {
-  switch (code) {
-  case ErrorCode::Ok: return "Ok";
-  case ErrorCode::InvalidValue: return "InvalidValue";
-  case ErrorCode::InvalidIndex: return "InvalidIndex";
-  case ErrorCode::AlreadyExists: return "AlreadyExists";
-  case ErrorCode::NotFound: return "NotFound";
-  case ErrorCode::Unimplemented: return "Unimplemented";
-  case ErrorCode::Unknown: return "Unknown";
-  default:
-    using UnderlyingCode = typename std::underlying_type<ErrorCode>::type;
-    const auto underlying_code = static_cast<UnderlyingCode>(code);
-    LOG(FATAL) << "Unrecognized ErrorCode: " << underlying_code;
-  }
-}
-
 // Forward declaration is required for sucessful overload lookup.
 // Otherwise, operator<<(stsd::ostream&, bool) is selected.
+template <typename Code>
 struct Result;
 
+template <typename Code>
 inline std::ostream& operator<<(std::ostream& out,
-                                const Result& result) noexcept;
+                                const Result<Code>& result) noexcept;
 
-inline constexpr bool operator==(const Result& lhs, const Result& rhs) noexcept;
+template <typename Code>
+inline constexpr bool operator==(const Result<Code>& lhs,
+                                 const Result<Code>& rhs) noexcept;
 
-struct Result : public trait::ToString<Result>, public trait::EqOps<Result> {
-  static constexpr Result CreateOk() noexcept { return Result(ErrorCode::Ok); }
+template <typename Code>
+struct Result : public trait::ToString<Result<Code>>,
+                public trait::EqOps<Result<Code>> {
+  template <typename Code::Enum code, typename... Args>
+  static constexpr Result Create(Args&&... args) noexcept {
+    return Result(Code::template Create<code>(), std::forward<Args>(args)...);
+  }
 
-  constexpr Result(ErrorCode code, StaticString message) noexcept
+  explicit constexpr Result(Code code, StaticString message = "") noexcept
     : code(std::move(code)), message(std::move(message)) {}
-  explicit constexpr Result(ErrorCode code) noexcept
-    : Result(std::move(code), "") {}
 
-  constexpr bool ok() const noexcept { return this->code == ErrorCode::Ok; }
+  constexpr bool ok() const noexcept {
+    return this->code.value == Code::Enum::Ok;
+  }
   constexpr operator bool() const noexcept { return this->ok(); }
   constexpr bool operator!() const noexcept {
     return !static_cast<bool>(*this);
   }
 
-  const ErrorCode code;
+  const Code code;
   const StaticString message;
 };
 
-static_assert(!std::is_default_constructible<Result>::value,
-              "Result should not be default-constructible");
-static_assert(std::is_copy_constructible<Result>::value,
-              "Result should be copy-constructible");
-static_assert(!std::is_copy_assignable<Result>::value,
-              "Result should not be copy-assignable");
-static_assert(std::is_move_constructible<Result>::value,
-              "Result should be move-constructible");
-static_assert(!std::is_move_assignable<Result>::value,
-              "Result should not be move-assignable");
-static_assert(std::is_literal_type<Result>::value,
-              "Result should be a literal type");
-
-std::ostream& operator<<(std::ostream& out, const Result& result) noexcept {
-  out << ErrorCodeToName(result.code);
+template <typename Code>
+std::ostream& operator<<(std::ostream& out,
+                         const Result<Code>& result) noexcept {
+  out << result.code;
   if (!result.message.empty()) {
     out << ": " << result.message;
   }
   return out;
 }
 
-constexpr bool operator==(const Result& lhs, const Result& rhs) noexcept {
+template <typename Code>
+constexpr bool operator==(const Result<Code>& lhs,
+                          const Result<Code>& rhs) noexcept {
   return lhs.code == rhs.code && lhs.message == rhs.message;
 }
 
+namespace internal {
+
+ENUM_CLASS(Code, Ok)
+using ResultAssert = Result<Code>;
+
+static_assert(!std::is_default_constructible<Result<Code>>::value,
+              "Result should not be default-constructible");
+static_assert(std::is_copy_constructible<Result<Code>>::value,
+              "Result should be copy-constructible");
+static_assert(!std::is_copy_assignable<Result<Code>>::value,
+              "Result should not be copy-assignable");
+static_assert(std::is_move_constructible<Result<Code>>::value,
+              "Result should be move-constructible");
+static_assert(!std::is_move_assignable<Result<Code>>::value,
+              "Result should not be move-assignable");
+static_assert(std::is_literal_type<Result<Code>>::value,
+              "Result should be a literal type");
+
+} // namespace internal
+
 } // namespace util
 
-#define _DO_CHECK_RESULT(result, expr)                                         \
+// Convenience macro to instantiate Result instances.
+#define RESULT(Code, value, ...)                                               \
+  Result<Code>::Create<Code::Enum::value>(__VA_ARGS__)
+
+// Check that a result is expression is "ok".
+#define _CHECK_RESULT_IMPL(result, expr)                                       \
   const auto result = (expr);                                                  \
   if (!result) CHECK_FAILED(#expr, result)
-#define CHECK_RESULT(expr) _DO_CHECK_RESULT(UNIQUE_VARIABLE_NAME(), expr)
+#define CHECK_RESULT(expr) _CHECK_RESULT_IMPL(UNIQUE_VARIABLE_NAME(), expr)
--- util/result_test.cpp
+++ util/result_test.cpp
@@ -3,68 +3,76 @@
 
 namespace util {
 
+ENUM_CLASS(TestCode, Ok, Error1, Error2)
+
 TEST(ResultTest, ok) {
-  {
-    const Result e = Result::CreateOk();
-    ASSERT_EQ(e.code, ErrorCode::Ok);
-    ASSERT_EQ(e.message, "");
-    ASSERT_TRUE(e.ok());
-    ASSERT_TRUE(static_cast<bool>(e));
-    ASSERT_FALSE(!e);
-  }
-
-  {
-    const Result e(ErrorCode::Ok, "Unique error message");
-    ASSERT_EQ(e.code, ErrorCode::Ok);
-    ASSERT_EQ(e.message, "Unique error message");
-    ASSERT_TRUE(e.ok());
-    ASSERT_TRUE(static_cast<bool>(e));
-    ASSERT_FALSE(!e);
-  }
+  const auto e = Result<TestCode>::Create<TestCode::Enum::Ok>();
+  ASSERT_EQ(e.code.value, TestCode::Enum::Ok);
+  ASSERT_EQ(e.message, "");
+  ASSERT_TRUE(e.ok());
+  ASSERT_TRUE(static_cast<bool>(e));
+  ASSERT_FALSE(!e);
 }
 
-TEST(ResultTest, notOk) {
-  {
-    const Result e(ErrorCode::InvalidValue);
-    ASSERT_EQ(e.code, ErrorCode::InvalidValue);
-    ASSERT_EQ(e.message, "");
-    ASSERT_FALSE(e.ok());
-    ASSERT_FALSE(static_cast<bool>(e));
-    ASSERT_TRUE(!e);
-  }
-
-  {
-    const Result e(ErrorCode::InvalidValue, "Unique error message");
-    ASSERT_EQ(e.code, ErrorCode::InvalidValue);
-    ASSERT_EQ(e.message, "Unique error message");
-    ASSERT_FALSE(e.ok());
-    ASSERT_FALSE(static_cast<bool>(e));
-    ASSERT_TRUE(!e);
-  }
+TEST(ResultTest, ok_message) {
+  const auto e = Result<TestCode>::Create<TestCode::Enum::Ok>("message");
+  ASSERT_EQ(e.code.value, TestCode::Enum::Ok);
+  ASSERT_EQ(e.message, "message");
+  ASSERT_TRUE(e.ok());
+  ASSERT_TRUE(static_cast<bool>(e));
+  ASSERT_FALSE(!e);
 }
 
-TEST(ResultTest, defaultErrorMessages) {
-  ASSERT_EQ(Result(ErrorCode::Ok).to_string(), "Ok");
-  ASSERT_EQ(Result(ErrorCode::InvalidValue).to_string(), "InvalidValue");
-  ASSERT_EQ(Result(ErrorCode::InvalidIndex).to_string(), "InvalidIndex");
-  ASSERT_EQ(Result(ErrorCode::AlreadyExists).to_string(), "AlreadyExists");
-  ASSERT_EQ(Result(ErrorCode::NotFound).to_string(), "NotFound");
-  ASSERT_EQ(Result(ErrorCode::Unimplemented).to_string(), "Unimplemented");
-  ASSERT_EQ(Result(ErrorCode::Unknown).to_string(), "Unknown");
+TEST(ResultTest, not_ok) {
+  const auto e = Result<TestCode>::Create<TestCode::Enum::Error1>();
+  ASSERT_EQ(e.code.value, TestCode::Enum::Error1);
+  ASSERT_EQ(e.message, "");
+  ASSERT_FALSE(e.ok());
+  ASSERT_FALSE(static_cast<bool>(e));
+  ASSERT_TRUE(!e);
+}
+
+TEST(ResultTest, not_ok_message) {
+  const auto e = Result<TestCode>::Create<TestCode::Enum::Error1>("message");
+  ASSERT_EQ(e.code.value, TestCode::Enum::Error1);
+  ASSERT_EQ(e.message, "message");
+  ASSERT_FALSE(e.ok());
+  ASSERT_FALSE(static_cast<bool>(e));
+  ASSERT_TRUE(!e);
+}
+
+TEST(ResultTest, default_error_messages) {
+  ASSERT_EQ(Result<TestCode>::Create<TestCode::Enum::Ok>().to_string(), "Ok");
+  ASSERT_EQ(Result<TestCode>::Create<TestCode::Enum::Error1>().to_string(),
+            "Error1");
+  ASSERT_EQ(Result<TestCode>::Create<TestCode::Enum::Error2>().to_string(),
+            "Error2");
+}
+
+TEST(ResultTest, explicit_error_message) {
+  ASSERT_EQ(Result<TestCode>::Create<TestCode::Enum::Ok>("message").to_string(),
+            "Ok: message");
+}
+
+TEST(ResultTest, macro) {
+  ASSERT_EQ(RESULT(TestCode, Ok),
+            Result<TestCode>::Create<TestCode::Enum::Ok>());
+  ASSERT_EQ(RESULT(TestCode, Error1, "message"),
+            Result<TestCode>::Create<TestCode::Enum::Error1>("message"));
 }
 
 TEST(ResultTest, check) {
-  CHECK(Result::CreateOk());
-  auto error = Result(ErrorCode::InvalidValue);
+  CHECK(Result<TestCode>::Create<TestCode::Enum::Ok>());
+  const auto error = Result<TestCode>::Create<TestCode::Enum::Error1>();
   ASSERT_DEATH({ CHECK(error.ok()) << "My message"; },
                ".*Check failed: error\\.ok\\(\\) My message");
 }
 
-TEST(ResultTest, checkOk) {
-  CHECK_RESULT(Result::CreateOk());
-  auto error = Result(ErrorCode::InvalidValue);
+TEST(ResultTest, check_result) {
+  CHECK_RESULT(Result<TestCode>::Create<TestCode::Enum::Ok>());
+  const auto error = Result<TestCode>::Create<TestCode::Enum::Error1>();
   ASSERT_DEATH({ CHECK_RESULT(error) << "My message"; },
-               ".*Check failed: error: InvalidValue: My message");
+               ".*Check failed: error: Error1: My message");
 }
 
 } // namespace util
