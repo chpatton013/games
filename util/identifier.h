3070037de71fe75401290d9e26d04b6e4a8e0015
Christopher Patton <chpatton013@gmail.com>
2017-02-25 16:25:14 -0800

    Util: revamp identifier

--- util/identifier.h
+++ util/identifier.h
@@ -1,43 +1,100 @@
 #pragma once
 
-#include <boost/optional.hpp>
+#include "trait/eq_ops.h"
+#include "trait/to_string.h"
 #include <cstdint>
 #include <glog/logging.h>
 #include <mutex>
+#include <ostream>
 #include <type_traits>
 #include <utility>
 
 namespace util {
 
-using Id = uint64_t;
+using Id = std::uint64_t;
+
+struct IdRange;
+
+inline constexpr bool operator==(const IdRange&, const IdRange&) noexcept;
+inline std::ostream& operator<<(std::ostream&, const IdRange&) noexcept;
+
+struct IdRange : public trait::EqOps<IdRange>, public trait::ToString<IdRange> {
+  constexpr IdRange(Id begin, Id end) noexcept
+    : begin(std::move(begin)), end(std::move(end)) {
+    CHECK_LE(begin, end) << "Invalid IdRange";
+  }
+
+  const Id begin;
+  const Id end;
+
+  constexpr std::size_t size() const noexcept {
+    return this->end - this->begin;
+  }
+
+  constexpr bool empty() const noexcept { return this->size() == 0; }
+};
+
+constexpr bool operator==(const IdRange& lhs, const IdRange& rhs) noexcept {
+  return lhs.begin == rhs.begin && lhs.end == rhs.end;
+}
+
+std::ostream& operator<<(std::ostream& out, const IdRange& range) noexcept {
+  return out << "IdRange[" << range.begin << "," << range.end << ")";
+}
 
 class Identifier {
 public:
-  static constexpr const Id kInvalidId = 0;
+  static constexpr Id InvalidId() noexcept { return 0; }
 
-  Identifier() : Identifier(1) {}
-  explicit Identifier(Id id) : id(std::move(id)), mutex() {}
+  Identifier() noexcept : Identifier(1) {}
+  explicit Identifier(Id id) noexcept : id(std::move(id)), mutex() {}
 
   Identifier(const Identifier&) = delete;
   Identifier& operator=(const Identifier&) = delete;
 
-  Identifier(Identifier&& that) : Identifier(std::move(that.id)) {
-    that.id = kInvalidId;
-  }
-  Identifier& operator=(Identifier&& that) {
+  Identifier(Identifier&& that) noexcept {
+    std::lock_guard<std::mutex> lock(that.mutex);
+
     this->id = std::move(that.id);
-    that.id = kInvalidId;
+    that.id = InvalidId();
+  }
+  Identifier& operator=(Identifier&& that) noexcept {
+    CHECK(this != &that) << "Cannot move-assign from the same instance";
+
+    std::lock_guard<std::mutex> this_lock(this->mutex);
+    std::lock_guard<std::mutex> that_lock(that.mutex);
+
+    this->id = std::move(that.id);
+    that.id = InvalidId();
     return *this;
   }
 
-  Id reserve() {
-    std::lock_guard<std::mutex> lock(mutex);
-    return id++;
+  bool valid() const noexcept { return this->id != InvalidId(); }
+
+  void check() const noexcept {
+    CHECK(this->valid()) << "Identifier is invalid";
   }
 
-  const Id& next() const {
-    std::lock_guard<std::mutex> lock(mutex);
-    return id;
+  Id last() const noexcept {
+    this->check();
+    return this->id - 1;
+  }
+
+  const Id& next() const noexcept {
+    this->check();
+    return this->id;
+  }
+
+  Id reserve() noexcept { return this->reserve(1).begin; }
+
+  IdRange reserve(std::size_t increment) noexcept {
+    CHECK(increment > 0) << "Cannot reserve 0 elements";
+    std::lock_guard<std::mutex> lock(this->mutex);
+    this->check();
+
+    const auto begin = this->id;
+    this->id += increment;
+    return IdRange(begin, this->id);
   }
 
 private:
--- util/identifier_test.cpp
+++ util/identifier_test.cpp
@@ -2,25 +2,129 @@
 #include <gtest/gtest.h>
 
 namespace util {
-namespace {
 
-TEST(IdentifierTest, id) {
-  {
-    Identifier id;
-
-    ASSERT_EQ(id.next(), 1);
-    ASSERT_EQ(id.reserve(), 1);
-    ASSERT_EQ(id.next(), 2);
-  }
-
-  {
-    Identifier id(5);
-
-    ASSERT_EQ(id.next(), 5);
-    ASSERT_EQ(id.reserve(), 5);
-    ASSERT_EQ(id.next(), 6);
-  }
+TEST(IdRangeTest, values) {
+  IdRange range(1, 3);
+  ASSERT_EQ(range.begin, 1);
+  ASSERT_EQ(range.end, 3);
+  ASSERT_EQ(range.size(), 2);
+  ASSERT_FALSE(range.empty());
 }
 
-} // namespace
+TEST(IdRangeTest, empty) {
+  IdRange range(1, 1);
+  ASSERT_EQ(range.size(), 0);
+  ASSERT_TRUE(range.empty());
+}
+
+TEST(IdRangeTest, reversed) {
+  ASSERT_DEATH({ IdRange range(2, 1); },
+               ".*Check failed: begin <= end \\(2 vs\\. 1\\) Invalid IdRange");
+}
+
+TEST(IdRangeTest, eq_ops) {
+  IdRange r1(1, 2);
+  IdRange r2(1, 3);
+  IdRange r3(2, 3);
+  ASSERT_EQ(r1, r1);
+  ASSERT_NE(r1, r2);
+  ASSERT_NE(r1, r3);
+  ASSERT_NE(r2, r1);
+  ASSERT_EQ(r2, r2);
+  ASSERT_NE(r2, r3);
+  ASSERT_NE(r3, r1);
+  ASSERT_NE(r3, r2);
+  ASSERT_EQ(r3, r3);
+}
+
+TEST(IdRangeTest, to_string) {
+  IdRange range(1, 3);
+  ASSERT_EQ(range.to_string(), "IdRange[1,3)");
+}
+
+TEST(IdentifierTest, default_ctor) {
+  Identifier id;
+  ASSERT_TRUE(id.valid());
+  ASSERT_EQ(id.last(), Identifier::InvalidId());
+  ASSERT_EQ(id.next(), 1);
+}
+
+TEST(IdentifierTest, specify_ctor) {
+  Identifier id(5);
+  ASSERT_TRUE(id.valid());
+  ASSERT_EQ(id.last(), 4);
+  ASSERT_EQ(id.next(), 5);
+}
+
+TEST(IdentifierTest, reserve) {
+  Identifier id;
+  ASSERT_EQ(id.reserve(), 1);
+  ASSERT_EQ(id.last(), 1);
+  ASSERT_EQ(id.next(), 2);
+  ASSERT_EQ(id.reserve(), 2);
+  ASSERT_EQ(id.last(), 2);
+  ASSERT_EQ(id.next(), 3);
+}
+
+TEST(IdentifierTest, reserve_range) {
+  Identifier id;
+  ASSERT_EQ(id.reserve(2), IdRange(1, 3));
+  ASSERT_EQ(id.last(), 2);
+  ASSERT_EQ(id.next(), 3);
+  ASSERT_EQ(id.reserve(2), IdRange(3, 5));
+  ASSERT_EQ(id.last(), 4);
+  ASSERT_EQ(id.next(), 5);
+}
+
+TEST(IdentifierTest, reserve_none) {
+  ASSERT_DEATH({ Identifier().reserve(0); },
+               ".*Check failed: increment > 0 Cannot reserve 0 elements");
+}
+
+TEST(IdentifierTest, move_ctor) {
+  Identifier id1(5);
+  Identifier id2(std::move(id1));
+  ASSERT_FALSE(id1.valid());
+  ASSERT_EQ(id2.next(), 5);
+}
+
+TEST(IdentifierTest, move_assign) {
+  Identifier id1(5);
+  Identifier id2 = std::move(id1);
+  ASSERT_FALSE(id1.valid());
+  ASSERT_EQ(id2.next(), 5);
+}
+
+TEST(IdentifierTest, move_same) {
+  Identifier id(5);
+  const auto remove_name = [](Identifier& id) -> Identifier& { return id; };
+  ASSERT_DEATH({ remove_name(id) = std::move(id); },
+               ".*Check failed: this != &that Cannot "
+               "move-assign from the same instance");
+}
+
+TEST(IdentifierTest, move_reserve) {
+  Identifier id1(5);
+  Identifier id2(std::move(id1));
+  ASSERT_FALSE(id1.valid());
+  ASSERT_DEATH({ id1.reserve(); },
+               ".*Check failed: this->valid\\(\\) Identifier is invalid");
+}
+
+TEST(IdentifierTest, move_last) {
+  Identifier id1(5);
+  Identifier id2(std::move(id1));
+  ASSERT_FALSE(id1.valid());
+  ASSERT_DEATH({ id1.last(); },
+               ".*Check failed: this->valid\\(\\) Identifier is invalid");
+}
+
+TEST(IdentifierTest, move_next) {
+  Identifier id1(5);
+  Identifier id2(std::move(id1));
+  ASSERT_FALSE(id1.valid());
+  ASSERT_DEATH({ id1.next(); },
+               ".*Check failed: this->valid\\(\\) Identifier is invalid");
+}
+
 } // namespace util
